3bc0b96e6ea94ca9157ed5e5ded4073152161829
Unknown <sahandhesar@gmail.com>
2022-03-01 01:28:47 +0100

    Fixed buggy packet

--- src/main/java/packets/outgoing/QuestRedeemPacket.java
+++ src/main/java/packets/outgoing/QuestRedeemPacket.java
@@ -1,25 +1,31 @@
-package packets.outgoing.arena;
+package packets.outgoing;
 
 import packets.Packet;
+import packets.data.QuestData;
 import packets.reader.BufferReader;
 import packets.data.SlotObjectData;
 
 /**
- * > Unknown.
+ * Packet sent when redeeming a quest.
  */
 public class QuestRedeemPacket extends Packet {
     /**
-     * > Unknown.
+     * Unknown string
      */
-    public String questId;
+    String unknownString;
     /**
-     * > Unknown.
+     * > Unknown int
+     */
+    public int questId;
+    /**
+     * > Unknown slots
      */
     public SlotObjectData[] slots;
 
     @Override
     public void deserialize(BufferReader buffer) throws Exception {
-        questId = buffer.readString();
+        unknownString = buffer.readString();
+        questId = buffer.readInt();
         slots = new SlotObjectData[buffer.readShort()];
         for (int i = 0; i < slots.length; i++) {
             slots[i] = new SlotObjectData().deserialize(buffer);
