ffa2906b1bef668f0f3c916c9e3627a1f31e358f
Unknown <sahandhesar@gmail.com>
2022-07-28 07:24:39 +0200

    Player shoot packet variable name changes

--- src/main/java/packets/outgoing/PlayerShootPacket.java
+++ src/main/java/packets/outgoing/PlayerShootPacket.java
@@ -12,18 +12,19 @@ public class PlayerShootPacket extends Packet {
      * The current client time.
      */
     public int time;
-     /**
+    /**
      * Counts the number of bullets sense entering dungeon.
      */
     public short bulletId;
     /**
      * The item id of the weapon used to fire the projectile.
      */
-    public int containerType;
+    public int weaponId;
     /**
-     * unknown byte
+     * ID of the projectile for weapons with multiple types of projectiles. (i.e. bows)
+     * Warning: some projectileIDs can be -1 and should be treated as 0!
      */
-    public byte unknownByte;
+    public byte projectileId;
     /**
      * The position of the starting point where the projectile was fired.
      */
@@ -41,8 +42,8 @@ public class PlayerShootPacket extends Packet {
     public void deserialize(BufferReader buffer) throws Exception {
         time = buffer.readInt();
         bulletId = buffer.readShort();
-        containerType = buffer.readUnsignedShort();
-        unknownByte = buffer.readByte();
+        weaponId = buffer.readUnsignedShort();
+        projectileId = buffer.readByte();
         startingPos = new WorldPosData().deserialize(buffer);
         angle = buffer.readFloat();
         isBurst = buffer.readBoolean();
@@ -53,8 +54,8 @@ public class PlayerShootPacket extends Packet {
         return "PlayerShootPacket{" +
                 "\n   time=" + time +
                 "\n   bulletID=" + bulletId +
-                "\n   containerType=" + containerType +
-                "\n   unknownByte=" + unknownByte +
+                "\n   weaponId=" + weaponId +
+                "\n   projectileId=" + projectileId +
                 "\n   startingPos=" + startingPos +
                 "\n   angle=" + angle +
                 "\n   isBurst=" + isBurst;
