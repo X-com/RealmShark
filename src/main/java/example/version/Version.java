67c225fe28181542147c944d632d0fc7daab5339
Unknown <sahandhesar@gmail.com>
2022-08-22 19:26:37 +0200

    More name change fixes

--- src/main/java/example/version/Version.java
+++
@@ -1,8 +0,0 @@
-package example.version;
-/**
- * Don't edit this class. It's a gradle class to grab version from the build.gradle
- */
-public class Version {
-    public static final String VERSION = "v1.5";
-}
-
