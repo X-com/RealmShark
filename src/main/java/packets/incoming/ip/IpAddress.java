f2c4bb18ec21c13fc01b601f41a2bf1cb7c45f46
Unknown <sahandhesar@gmail.com>
2022-09-17 10:06:51 +0200

    Added server naming convention of realm nexus IPs

--- src/main/java/packets/incoming/ip/IpAddress.java
+++ src/main/java/packets/incoming/ip/IpAddress.java
@@ -19,6 +19,10 @@ public class IpAddress extends Packet {
      * IP of incoming packets as a single integer
      */
     public int srcAddressAsInt;
+    /**
+     * The IP address server name, i.e. USSouth, EUWest2.
+     */
+    public String ipAddressName;
 
     public IpAddress() {
     }
@@ -27,6 +31,68 @@ public class IpAddress extends Packet {
         super();
         srcAddress = srcAddr;
         srcAddressAsInt = Util.decodeInt(srcAddr);
+        setName(srcAddressAsInt);
+    }
+
+    private void setName(int ip) {
+        switch (ip) {
+            case 921430924:
+                ipAddressName = "USWest4";
+                break;
+            case 311434905:
+                ipAddressName = "USWest3";
+                break;
+            case 911617968:
+                ipAddressName = "USWest";
+                break;
+            case 916000068:
+                ipAddressName = "USSouthWest";
+                break;
+            case 886033951:
+                ipAddressName = "USSouth3";
+                break;
+            case 55737872:
+                ipAddressName = "USSouth";
+                break;
+            case 586068087:
+                ipAddressName = "USNorthWest";
+                break;
+            case 59571845:
+                ipAddressName = "USMidWest2";
+                break;
+            case 59051031:
+                ipAddressName = "USMidWest";
+                break;
+            case 878180357:
+                ipAddressName = "USEast2";
+                break;
+            case 921362968:
+                ipAddressName = "USEast";
+                break;
+            case 873486039:
+                ipAddressName = "EUWest2";
+                break;
+            case 267205855:
+                ipAddressName = "EUWest";
+                break;
+            case 599016312:
+                ipAddressName = "EUSouthWest";
+                break;
+            case 312444280:
+                ipAddressName = "EUNorth";
+                break;
+            case 314104494:
+                ipAddressName = "EUEast";
+                break;
+            case 233592826:
+                ipAddressName = "Australia";
+                break;
+            case 50369407:
+                ipAddressName = "Asia";
+                break;
+            default:
+                ipAddressName = "";
+        }
     }
 
     @Override
@@ -36,7 +102,8 @@ public class IpAddress extends Packet {
     @Override
     public String toString() {
         return "IpAddress{" +
-                "\n   srcAddress=" + Arrays.toString(srcAddress) +
-                "\n   srcAddressAsInt=" + srcAddressAsInt;
+                "\n   srcAddress=" + String.format("%d.%d.%d.%d", Byte.toUnsignedInt(srcAddress[0]), Byte.toUnsignedInt(srcAddress[1]), Byte.toUnsignedInt(srcAddress[2]), Byte.toUnsignedInt(srcAddress[3])) +
+                "\n   srcAddressAsInt=" + srcAddressAsInt +
+                "\n   ipAddressName=" + ipAddressName;
     }
 }
