62177f577db9d57ed5939e30c53ddd7d20b9fa8e
Unknown <sahandhesar@gmail.com>
2022-10-01 22:57:17 +0200

    Updates to potato server

--- potato server/main.js
+++ potato server/main.js
@@ -1,96 +1,94 @@
 // Importing the required modules
 const WebSocketServer = require('ws');
-const port = 6000
-const logger = true
+const port = 6000;
+const logger = false;
 // Creating a new websocket server
-const wss = new WebSocketServer.Server({ port: port })
+const wss = new WebSocketServer.Server({ port: port });
 
-const servers = new Map()
-const userServer = new Map()
+const servers = new Map();
+const userServer = new Map();
+const fs = require('fs/promises');
+
+let nextUserId = 0;
 
 // Creating connection using websocket
 wss.on("connection", (ws) => {
-        const id = uuidv4();
-        if (logger) console.log("new client connected " + id);
+        const id = nextUserId;
+        nextUserId++;
     ws.on("message", data => {
-        let json = JSON.parse(data)
+        let json = JSON.parse(data);
         if (json.packet == "hero") {
-            const server = userServer.get(id)
-            server.markers[json.heroId] = json.state
-            string = JSON.stringify(json)
+            const server = userServer.get(id);
+            server.markers[json.heroId] = json.state;
+            string = JSON.stringify(json);
             server.subs.forEach((client) => {
-                if (client != ws) {
-                    client.send(string)
+                if (client !== ws) {
+                    client.send(string);
                 }
             });
-            if (logger) console.log("hero " + id)
-        } else if (json.packet == "sub") {
+            server.edited = true;
+        } else if (json.packet === "sub") {
             let server = servers.get(json.ip);
 
-            if (server == null) {
-                server = new Server()
-                server.map = json.map
-                server.seed = json.seed
-                server.markers = new Array(92).fill(0)
-                server.subs = new Set()
-                servers.set(json.ip, server)
-            } else if (server.seed != json.seed) {
-                server.seed = json.seed
-                server.map = json.map
-                server.markers.fill(0)
+            if (server === undefined) {
+                server = {
+                    map: json.map,
+                    seed: json.seed,
+                    markers: new Array(92).fill(0),
+                    subs: new Set(),
+                    ip: json.ip,
+                    edited: false,
+                };
+                servers.set(json.ip, server);
+            } else if (server.seed !== json.seed) {
+                server.ip = json.ip;
+                server.seed = json.seed;
+                server.map = json.map;
+                server.markers.fill(0);
+                server.edited = false;
             }
-            userServer.set(id, server)
-            server.subs.add(ws)
+            userServer.set(id, server);
+            server.subs.add(ws);
 
-            let send = new Sender()
-            send.packet = "subbed"
-            send.map = server.map
-            send.markers = server.markers
+            let send = {
+                packet: "subbed",
+                map: server.map,
+                markers: server.markers,
+            };
 
-            string = JSON.stringify(send)
-            ws.send(string)
-            if (logger) console.log("sub " + id)
+            write(`sub server:${server.ip} seed:${json.seed} map:${json.map + 1} x:${json.x} y:${json.y}\n`);
+            ws.send(JSON.stringify(send));
         } else if (json.packet == "unsub") {
-            const server = userServer.get(id)
-            if (server != null && server.subs != null) {
-                server.subs.delete(ws)
-                userServer.delete(id)
-                if (logger) console.log("unsub " + id)
+            const server = userServer.get(id);
+            if (server !== undefined && server.subs !== undefined) {
+                server.subs.delete(ws);
+                userServer.delete(id);
+                if (server.edited && server.subs.size === 0) {
+                    write(`dat server:${server.ip}map:${server.map + 1}seed:${server.seed}m:${server.markers}\n`);
+                    server.edited = false;
+                }
             }
         }
     });
     // handling what to do when clients disconnects from server
     ws.on("close", () => {
-        const server = userServer.get(id)
-        if (server != null && server.subs != null) {
-            server.subs.delete(ws)
-            userServer.delete(id)
+        const server = userServer.get(id);
+        if (server !== undefined && server.subs !== undefined) {
+            server.subs.delete(ws);
+            userServer.delete(id);
         }
-        if (logger) console.log("client disconnected " + id);
     });
     // handling client connection error
-    ws.onerror = function () {
-        if (logger) console.log("Some Error occurred")
+    ws.onerror = () => {
+        console.log("Some Error occurred");
     }
 });
 console.log("The WebSocket server is running on port " + port);
 
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
-class Sender {
-    packet
-    map
-    markers
-}
-
-class Server {
-    map
-    seed
-    markers
-    subs
+const write = async (content) => {
+    try {
+        await fs.appendFile(__dirname + '/log.txt', content);
+    } catch (err) {
+        console.log(err);
+    }
 }
