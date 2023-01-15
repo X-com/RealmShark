d2aa81ca0afea435cbeffe8ae1ecf53d4bc938b8
Unknown <sahandhesar@gmail.com>
2023-01-15 20:29:08 +0100

    Bumped to v0.5.4 beta

--- src/main/java/potato/version/Version.java
+++ src/main/java/potato/version/Version.java
@@ -3,6 +3,6 @@ package potato.version;
  * Don't edit this class. It's a gradle class to grab version from the build.gradle
  */
 public class Version {
-    public static final String VERSION = "v0.4.3-beta";
+    public static final String VERSION = "v0.5.4-beta";
 }
 
