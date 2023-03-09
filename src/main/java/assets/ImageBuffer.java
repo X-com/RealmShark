9c0f86aa67d3fec3b649fd59f016fc38805123c8
Unknown <sahandhesar@gmail.com>
2023-03-10 00:32:45 +0100

    Fixed proper sprite and json asset paths

---
+++ src/main/java/assets/ImageBuffer.java
@@ -0,0 +1,70 @@
+package assets;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Buffered image class used to crop images from the sprite atlas.
+ */
+public class ImageBuffer {
+
+    private static HashMap<Integer, BufferedImage> images = new HashMap<>();
+    private static HashMap<Integer, Integer> colors = new HashMap<>();
+    private static final SpriteJson spriteJson = new SpriteJson();
+
+    private static String[] spriteSheets = {"assets/sprite/tiles.png", "assets/sprite/characters.png", "assets/sprite/characters_masks.png", "assets/sprite/objects.png"};
+    private static BufferedImage[] bigImages = new BufferedImage[4];
+
+    /**
+     * Image method used to get image from object id.
+     *
+     * @param id Type ID of the object.
+     * @return Sprite of based of the object type ID.
+     * @throws IOException Thrown if the sprite atlas file is missing.
+     */
+    public static BufferedImage getImage(int id) throws IOException, AssetMissingException {
+        if (id <= 0) return null;
+        if (images.containsKey(id)) return images.get(id);
+        String name = IdToAsset.getObjectTextureName(id, 0);
+        int index = IdToAsset.getObjectTextureIndex(id, 0);
+        int[] spriteData = spriteJson.getSprite(name, index);
+        BufferedImage sprite = getSprite(spriteData);
+        images.put(id, sprite);
+        return sprite;
+    }
+
+    /**
+     * Color method used to get most common color from tile id.
+     *
+     * @param id Type ID of the tile.
+     * @return Most common color of the tile.
+     */
+    public static int getColor(int id) throws AssetMissingException {
+        if (id <= 0) return 0;
+        if (colors.containsKey(id)) return colors.get(id);
+        if (!IdToAsset.tileIdExists(id)) return 0;
+        String name = IdToAsset.getTileTextureName(id, 0);
+        if (name == null) return 0;
+        int index = IdToAsset.getTileTextureIndex(id, 0);
+        int color = spriteJson.getSpriteColor(name, index);
+        colors.put(id, color);
+        return color;
+    }
+
+    /**
+     * Retrieves the sprite from the sprite atlas based on image location on the atlas.
+     *
+     * @param data The coordinate, size and altas ID of the sprite being requested.
+     * @return Buffered image of the sprite cropped from a sprite atlas.
+     * @throws IOException Thrown if the sprite atlas file is missing.
+     */
+    private static BufferedImage getSprite(int[] data) throws IOException { // data:{x, y, w, h, aId}
+        if (data == null) return null;
+        if (bigImages[data[4] - 1] == null) bigImages[data[4] - 1] = ImageIO.read(new File(spriteSheets[data[4] - 1]));
+
+        return bigImages[data[4] - 1].getSubimage(data[0], data[1], data[2], data[3]);
+    }
+}
---
+++ src/main/java/assets/SpriteJson.java
@@ -0,0 +1,251 @@
+package assets;
+
+import static com.google.gson.FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES;
+
+import com.google.gson.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Type;
+import java.util.*;
+
+/**
+ * Json parser specific made for realm sprite json.
+ */
+public class SpriteJson implements JsonDeserializer<SpriteJson> {
+    private static String spriteJson = "assets/json/spritesheet.json";
+
+    private static HashMap<String, HashMap<Integer, Sprite>> sprites;
+    private static HashMap<String, Sprite> animatedSprites;
+
+    /**
+     * Static class used to load the json file and parse the json.
+     */
+    static {
+        try {
+            GsonBuilder builder = new GsonBuilder();
+            builder.registerTypeAdapter(SpriteJson.class, new SpriteJson());
+            builder.registerTypeAdapter(Sprite.class, new Sprite());
+            Gson gson = builder.setFieldNamingPolicy(LOWER_CASE_WITH_UNDERSCORES).create();
+            String json = new Scanner(new File(spriteJson)).useDelimiter("\\Z").next();
+            gson.fromJson(json, SpriteJson.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Simple constructor used in json parsing.
+     */
+    public SpriteJson() {
+    }
+
+    /**
+     * Main constructor used for constructing the json sprite data.
+     *
+     * @param sprites         Hash list of all sprites.
+     * @param animatedSprites Hash list of all animated sprites. (currently not implemented fully)
+     */
+    public SpriteJson(HashMap<String, HashMap<Integer, Sprite>> sprites, HashMap<String, Sprite> animatedSprites) {
+        this.sprites = sprites;
+        this.animatedSprites = animatedSprites;
+    }
+
+    /**
+     * Retrieves sprite coordinates used in sprite atlases based on sprite group name and index.
+     *
+     * @param name  Name of the sprite group.
+     * @param index Index of the sprite in the group.
+     * @return Integer
+     */
+    public int[] getSprite(String name, int index) {
+        HashMap<Integer, Sprite> list = sprites.get(name);
+        Sprite sprite = list.get(index);
+        return new int[]{sprite.position.x, sprite.position.y, sprite.position.w, sprite.position.h, sprite.aId};
+    }
+
+    /**
+     * Retrieves sprite most common color.
+     *
+     * @param name  Name of the sprite group.
+     * @param index Index of the sprite in the group.
+     * @return Integer
+     */
+    public int getSpriteColor(String name, int index) {
+        HashMap<Integer, Sprite> list = sprites.get(name);
+        Sprite sprite = list.get(index);
+        return sprite.mostCommonColor.asInt();
+    }
+
+    /**
+     * Returns all sprite data parsed by the json.
+     *
+     * @return Hash of the sprite data.
+     */
+    public HashMap<String, HashMap<Integer, Sprite>> getSprites() {
+        return sprites;
+    }
+
+
+    /**
+     * Returns all animated sprite data parsed by the json.
+     *
+     * @return Hash of the animated sprite data.
+     */
+    public HashMap<String, Sprite> getAnimatedSprites() {
+        return animatedSprites;
+    }
+
+    /**
+     * Json parser used to unwrap the Unity sprite json.
+     */
+    @Override
+    public SpriteJson deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+        HashMap<String, HashMap<Integer, Sprite>> sprites = new HashMap<>(); // sprites
+        HashMap<String, Sprite> animatedSprites = new HashMap<>(); // animatedSprites
+        JsonObject jsonObject = element.getAsJsonObject();
+
+        JsonArray spritesArray = jsonObject.getAsJsonArray("sprites");
+        for (JsonElement spriteArray : spritesArray) {
+            JsonObject spriteObject = spriteArray.getAsJsonObject();
+            String name = spriteObject.getAsJsonPrimitive("spriteSheetName").getAsString();
+            JsonArray elements = spriteObject.getAsJsonArray("elements");
+            HashMap<Integer, Sprite> list = new HashMap<>();
+            for (JsonElement spriteElement : elements) {
+                Sprite sprite = context.deserialize(spriteElement, Sprite.class);
+                if (!list.containsKey(sprite.index)) {
+                    list.put(sprite.index, sprite);
+                } else {
+                    throw new RuntimeException("Double insertion to same sprite index.");
+                }
+            }
+            sprites.put(name, list);
+        }
+
+        // TODO: fix animated sprites.
+//        JsonArray animatedArray = jsonObject.getAsJsonArray("animatedSprites");
+//        for (JsonElement animated : animatedArray) {
+//            JsonObject animatedObject = animated.getAsJsonObject();
+//            int index = animatedObject.getAsJsonPrimitive("index").getAsInt();
+//            String name = animatedObject.getAsJsonPrimitive("spriteSheetName").getAsString();
+//            int direction = animatedObject.getAsJsonPrimitive("direction").getAsInt();
+//            String action = animatedObject.getAsJsonPrimitive("action").getAsString();
+//            int set = animatedObject.getAsJsonPrimitive("set").getAsInt();
+//            JsonObject spriteData = animatedObject.getAsJsonObject("spriteData");
+//            Sprite sprite = context.deserialize(spriteData, Sprite.class);
+//            sprite.setAnimationVars(index, direction, action, set);
+//            animatedSprites.put(name, sprite);
+//        }
+
+        return new SpriteJson(sprites, animatedSprites);
+    }
+
+    /**
+     * Sprite class used to store sprite atlas coordinate data parsed by the json.
+     */
+    static class Sprite implements JsonDeserializer<Sprite> {
+        int padding;
+        int index;
+        int aId;
+        boolean isT;
+        ImgCoords position;
+        ImgCoords maskPosition;
+        Color mostCommonColor;
+
+        int animatedIndex;
+        int animatedDirection;
+        String animatedAction;
+        int animatedSet;
+
+        public void setAnimationVars(int index, int direction, String action, int set) {
+            animatedIndex = index;
+            animatedDirection = direction;
+            animatedAction = action;
+            animatedSet = set;
+        }
+
+        @Override
+        public Sprite deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            Sprite s = new Sprite();
+            JsonObject jsonObject = json.getAsJsonObject();
+
+            s.padding = jsonObject.getAsJsonPrimitive("padding").getAsInt();
+            s.index = jsonObject.getAsJsonPrimitive("index").getAsInt();
+            s.aId = jsonObject.getAsJsonPrimitive("aId").getAsInt();
+            s.isT = jsonObject.getAsJsonPrimitive("isT").getAsBoolean();
+
+            JsonElement pos = jsonObject.getAsJsonObject("position");
+            s.position = new ImgCoords();
+            s.position.x = pos.getAsJsonObject().getAsJsonPrimitive("x").getAsInt();
+            s.position.y = pos.getAsJsonObject().getAsJsonPrimitive("y").getAsInt();
+            s.position.w = pos.getAsJsonObject().getAsJsonPrimitive("w").getAsInt();
+            s.position.h = pos.getAsJsonObject().getAsJsonPrimitive("h").getAsInt();
+
+            JsonElement mask = jsonObject.getAsJsonObject("maskPosition");
+            s.maskPosition = new ImgCoords();
+            s.maskPosition.x = mask.getAsJsonObject().getAsJsonPrimitive("x").getAsInt();
+            s.maskPosition.y = mask.getAsJsonObject().getAsJsonPrimitive("y").getAsInt();
+            s.maskPosition.w = mask.getAsJsonObject().getAsJsonPrimitive("w").getAsInt();
+            s.maskPosition.h = mask.getAsJsonObject().getAsJsonPrimitive("h").getAsInt();
+
+            JsonElement color = jsonObject.getAsJsonObject("mostCommonColor");
+            s.mostCommonColor = new Color();
+            s.mostCommonColor.r = color.getAsJsonObject().getAsJsonPrimitive("r").getAsInt();
+            s.mostCommonColor.g = color.getAsJsonObject().getAsJsonPrimitive("g").getAsInt();
+            s.mostCommonColor.b = color.getAsJsonObject().getAsJsonPrimitive("b").getAsInt();
+            s.mostCommonColor.a = color.getAsJsonObject().getAsJsonPrimitive("a").getAsInt();
+
+//            for (Map.Entry<String, JsonElement> entry : pos.getAsJsonObject().entrySet()) {
+//                System.out.println(entry.getKey());
+//            }
+//            JsonArray spritesArray = jsonObject.getAsJsonArray("sprites");
+
+            return s;
+        }
+
+        class ImgCoords {
+            int x;
+            int y;
+            int w;
+            int h;
+
+            @Override
+            public String toString() {
+                StringBuilder sb = new StringBuilder();
+                sb.append(String.format("(x:%d, y:%d, w:%d, h:%d)", x, y, w, h));
+                return sb.toString();
+            }
+        }
+
+        /**
+         * Color class storing basic color data.
+         */
+        class Color {
+            int r;
+            int g;
+            int b;
+            int a;
+
+            @Override
+            public String toString() {
+                StringBuilder sb = new StringBuilder();
+                sb.append(String.format("(r:%d, g:%d, b:%d, a:%d)", r, g, b, a));
+                return sb.toString();
+            }
+
+            public int asInt() {
+                return a << 24 | r << 16 | g << 8 | b;
+            }
+        }
+
+        /**
+         * To string for debuging Json parsing.
+         */
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("padding:%d, index:%d, aId%d, isT:%b, position:%s, maskPosition:%s, mostCommonColor:%s", padding, index, aId, isT, position, maskPosition, mostCommonColor));
+            return sb.toString();
+        }
+    }
+}
