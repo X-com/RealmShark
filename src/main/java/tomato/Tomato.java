14d0497c4b3042a822fc2fc63341373f21ef3426
Unknown <sahandhesar@gmail.com>
2023-04-01 16:14:43 +0200

    Tomato main crash system handling

--- src/main/java/tomato/Tomato.java
+++ src/main/java/tomato/Tomato.java
@@ -18,6 +18,12 @@ import assets.AssetMissingException;
 import assets.IdToAsset;
 import util.Util;
 
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -42,8 +48,34 @@ public class Tomato {
 
     public static void main(String[] args) {
         Util.setSaveLogs(true); // turns the logger to, save in to files.
-        Tomato.packetRegister();
-        new TomatoGUI().create();
+        try {
+            Tomato.packetRegister();
+            new TomatoGUI().create();
+        } catch (OutOfMemoryError e) {
+            crashDialog();
+        } catch (Exception e) {
+            Util.print("Main crash:");
+            Util.print(e.getMessage());
+        }
+    }
+
+    /**
+     * Dialog to display crash if tomato runs out of memory.
+     * Most memory crashes related to 32-bit java suggesting 64-bit install.
+     */
+    private static void crashDialog() {
+        JEditorPane ep = new JEditorPane("text/html", "<html>Out of memory crash.<br>Please uninstall 32-bit and install 64-bit Java.<br><a href=\\\\\\\"https://www.java.com/en/download/manual.jsp\\\\\\\">Download java 64-bit</a></html>");
+        ep.addHyperlinkListener(e -> {
+            if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://www.java.com/en/download/manual.jsp"));
+                } catch (IOException | URISyntaxException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        ep.setEditable(false);
+        JOptionPane.showMessageDialog(null, ep);
     }
 
     /**
