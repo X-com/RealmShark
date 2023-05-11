0fbe72ea46ee8ba7912ee99919921038a07bd795
Unknown <sahandhesar@gmail.com>
2023-05-11 08:57:21 +0200

    Implemented error logger and error display

---
+++ src/main/java/tomato/logic/backend/CrashLogger.java
@@ -0,0 +1,31 @@
+package tomato.logic.backend;
+
+import util.Util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Crash logger, storing crash data to file.
+ */
+public class CrashLogger {
+
+    /**
+     * Logger for crashes captured by the exception handler.
+     *
+     * @param error Error to be logged.
+     */
+    public static void printCrash(Exception error) {
+        Util.print("Main crash:");
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        error.printStackTrace(pw);
+        Util.print(sw.toString());
+    }
+
+    /**
+     * Class loader method in java. Loading class here in case of stackoverflow errors.
+     */
+    public static void loadThisClass() {
+    }
+}
