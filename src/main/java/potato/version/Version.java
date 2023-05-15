fc5b4ae355fbdc3c07a46e8697c4d711bbfef170
Unknown <sahandhesar@gmail.com>
2023-05-15 14:45:31 +0200

    v0.9

--- src/main/java/potato/version/Version.java
+++ src/main/java/potato/version/Version.java
@@ -3,6 +3,6 @@ package potato.version;
  * Don't edit this class. It's a gradle class to grab version from the build.gradle
  */
 public class Version {
-    public static final String VERSION = "v0.8.1";
+    public static final String VERSION = "v0.9";
 }
 
