d861dbf7778fb68a11c2a7aa79d4b91de3823321
Unknown <sahandhesar@gmail.com>
2023-07-14 12:11:11 +0200

    Fixed packet 165

--- src/main/java/packets/incoming/UnknownPacket165.java
+++ src/main/java/packets/incoming/UnknownPacket165.java
@@ -3,32 +3,25 @@ package packets.incoming;
 import packets.Packet;
 import packets.reader.BufferReader;
 
+import javax.swing.plaf.synth.SynthUI;
+
 /**
- * Packets to do with quest updates from seasonal quests.
+ * Unknown packet -91 / 165
  */
 public class UnknownPacket165 extends Packet {
     /**
      * Unknown byte
      */
-    public int unknownByte;
-    /**
-     * Unknown int
-     */
-    public int unknownInt1;
-    public int unknownInt2;
+    public String unknownString;
 
     @Override
     public void deserialize(BufferReader buffer) throws Exception {
-        unknownByte = buffer.readByte();
-        unknownInt1 = buffer.readInt();
-        unknownInt2 = buffer.readInt();
+        unknownString = buffer.readString();
     }
 
     @Override
     public String toString() {
-        return "UnknownPacket147{" +
-                "\n   unknownByte=" + unknownByte +
-                "\n   unknownInt1=" + unknownInt1 +
-                "\n   unknownInt2=" + unknownInt2;
+        return "UnknownPacket165{" +
+                "\n   unknownString=" + unknownString;
     }
 }
