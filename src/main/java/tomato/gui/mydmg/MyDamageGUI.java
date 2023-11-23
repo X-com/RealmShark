3ceca12089c142e6c920dfd1fc591ff408010873
Xcom <sahandhesar@gmail.com>
2023-11-24 00:22:57 +0100

    Improved my dmg tab

--- src/main/java/tomato/gui/mydmg/MyDamageGUI.java
+++ src/main/java/tomato/gui/mydmg/MyDamageGUI.java
@@ -20,6 +20,9 @@ public class MyDamageGUI extends JPanel {
     private int[] slots;
     private JTextArea textArea;
     private Entity pet;
+    private static final float[] petRegenTimeMpHp = {10.00f, 9.35f, 8.69f, 8.04f, 7.39f, 7.13f, 6.88f, 6.62f, 6.37f, 6.11f, 5.94f, 5.76f, 5.59f, 5.42f, 5.25f, 5.13f, 5.01f, 4.90f, 4.78f, 4.67f, 4.58f, 4.49f, 4.40f, 4.31f, 4.22f, 4.14f, 4.06f, 3.98f, 3.90f, 3.82f, 3.75f, 3.69f, 3.62f, 3.55f, 3.48f, 3.43f, 3.39f, 3.34f, 3.29f, 3.24f, 3.19f, 3.13f, 3.08f, 3.03f, 2.97f, 2.91f, 2.85f, 2.78f, 2.72f, 2.66f, 2.61f, 2.56f, 2.51f, 2.46f, 2.41f, 2.38f, 2.34f, 2.30f, 2.26f, 2.22f, 2.19f, 2.15f, 2.12f, 2.08f, 2.05f, 2.03f, 2.01f, 1.99f, 1.98f, 1.96f, 1.93f, 1.91f, 1.88f, 1.85f, 1.83f, 1.79f, 1.74f, 1.70f, 1.65f, 1.61f, 1.57f, 1.53f, 1.50f, 1.46f, 1.42f, 1.42f, 1.42f, 1.42f, 1.42f, 1.42f, 1.38f, 1.34f, 1.29f, 1.25f, 1.21f, 1.17f, 1.13f, 1.08f, 1.04f, 1.00f};
+    private static final int[] petManaPerLevel = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 11, 11, 11, 12, 12, 13, 13, 14, 14, 15, 16, 16, 17, 17, 18, 19, 19, 20, 21, 21, 22, 23, 24, 24, 25, 26, 27, 28, 28, 29, 30, 31, 32, 33, 34, 35, 36, 38, 39, 40, 41, 42, 44, 45};
+    private static final int[] petHpPerLevel = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 13, 13, 13, 14, 14, 14, 14, 15, 15, 16, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25, 26, 26, 27, 28, 29, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 46, 47, 48, 50, 51, 53, 54, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 88, 90};
 
     public MyDamageGUI(TomatoData data) {
         INSTANCE = this;
@@ -89,6 +92,7 @@ public class MyDamageGUI extends JPanel {
             sb.append(String.format("Bullet: %sx  %s - %s   Rof: %s  Dps: %.1f\n", b.numProj, b.min, b.max, Float.toString(b.rof), dps));
             total += dps;
         }
+        sb.append(String.format("Weapon %.1f dmg/sec\n", total));
         sb.append("\n");
         if (pet != null) {
             int electric = getPetStat(406);
@@ -136,8 +140,10 @@ public class MyDamageGUI extends JPanel {
             int level = getPetStat(408);
 
             if (level != -1) {
-                int mana = (int) (1.9f * Math.exp(0.0318f * level));
-                float time = (float) (10f - 4.5f * Math.log10(level));
+//                int mana = (int) (1.9f * Math.exp(0.0318f * level));
+//                float time = (float) (10f - 4.5f * Math.log10(level));
+                int mana = petManaPerLevel[level - 1];
+                float time = petRegenTimeMpHp[level - 1];
                 petManaRegen = mana / time;
                 sb.append(String.format("Pet M.Heal level: %d   %.2f mana/sec   [mana: %d per %.2f sec]\n", level, petManaRegen, mana, time));
             }
