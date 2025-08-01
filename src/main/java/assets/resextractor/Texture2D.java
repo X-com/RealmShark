336a44c320b691a91adc2df8930d8d3b4eecfb34
Xcom <sahandhesar@gmail.com>
2025-08-01 21:58:26 +0200

    Correctly fixed texture parsing

--- src/main/java/assets/resextractor/Texture2D.java
+++ src/main/java/assets/resextractor/Texture2D.java
@@ -57,10 +57,10 @@ public class Texture2D {
         name = reader.readAlignedString();
 
 //        if self.version >= (2017, 3){
-        m_ForcedFallbackFormat = reader.readInt();
+//        m_ForcedFallbackFormat = reader.readInt();
 //        m_DownscaleFallback = reader.readBoolean();
 //        if self.version >= (2020,2): # 2020.2 and up {
-//        m_IsAlphaChannelOptional = reader.readBoolean();
+        m_IsAlphaChannelOptional = reader.readBoolean();
 //        }
         reader.alignStream();
 
