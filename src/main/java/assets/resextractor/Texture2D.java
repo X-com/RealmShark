91a20d2925404442bc49bb319093c97bf88ee09d
Xcom <sahandhesar@gmail.com>
2025-08-01 03:21:09 +0200

    Temp fixed texture extractor parser

--- src/main/java/assets/resextractor/Texture2D.java
+++ src/main/java/assets/resextractor/Texture2D.java
@@ -58,11 +58,12 @@ public class Texture2D {
 
 //        if self.version >= (2017, 3){
         m_ForcedFallbackFormat = reader.readInt();
-        m_DownscaleFallback = reader.readBoolean();
+//        m_DownscaleFallback = reader.readBoolean();
 //        if self.version >= (2020,2): # 2020.2 and up {
-        m_IsAlphaChannelOptional = reader.readBoolean();
+//        m_IsAlphaChannelOptional = reader.readBoolean();
 //        }
         reader.alignStream();
+
 //        }
 
         m_Width = reader.readInt();
