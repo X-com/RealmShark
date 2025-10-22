c529ffdaf70bb4112aa1eb0f6b3324435b9f81f7
Alex Arze <alexarze@gmail.com>
2025-10-21 19:23:20 -0500

    Version to 1.2

--- src/main/java/realmshark/version/Version.java
+++ src/main/java/realmshark/version/Version.java
@@ -3,6 +3,6 @@ package realmshark.version;
  * Don't edit this class. It's a gradle class to grab version from the build.gradle
  */
 public class Version {
-    public static final String VERSION = "v1.1";
+    public static final String VERSION = "v1.2";
 }
 
